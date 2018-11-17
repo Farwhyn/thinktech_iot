bbc019831d0ec18f04c8202b51ed3577a9770d0b
Farwhyn <jamesizhou@gmail.com>
2018-11-16 23:25:00 -0800

    removed useless files

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -204,7 +204,3 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/* setInterval(function() {
-  var newdata = generateRandomData();
-  init(newdata);
-}, 1000); */
